a257c3e7925ee1d19927dd8762ef0b11312d25de
Armin Sander <armin@replicator.org>
2015-03-04 16:17:49 +0100

    review assembly infos

--- FsReact.Tests/AssemblyInfo.fs
+++ FsReact.Tests/AssemblyInfo.fs
@@ -1,21 +1,16 @@
 ﻿module FsReact.iOS.Tests.AssemblyInfo
 open System.Reflection
-open System.Runtime.CompilerServices
 
 [<assembly: AssemblyTitle("FsReact.Tests")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyCompany("Armin Sander")>]
 [<assembly: AssemblyProduct("FsReact.Tests")>]
 [<assembly: AssemblyCopyright("Copyright © 2015 Armin Sander")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
-// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
 
-[<assembly: AssemblyVersion("1.0.0.0")>]
-
-//[<assembly: AssemblyDelaySign(false)>]
-//[<assembly: AssemblyKeyFile("")>]
+[<assembly: AssemblyVersion("0.1.0.0")>]
 
 ()
 
